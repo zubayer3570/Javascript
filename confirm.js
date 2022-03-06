79df9b11013d757d05816a8ffe02908c6556724c
zubayer <zubayer3570@gmail.com>
2022-03-06 11:36:59 +0600

    hi

--- confirm.js
+++ confirm.js
@@ -1,9 +1,11 @@
 document.getElementById('confirm').addEventListener('click', function () {
     const response = confirm("let's go to saint martin with me")
     if (response) {
+        document.getElementById('cancel').innerText = ''
         document.getElementById('ok').innerText = 'ok cholooooo😘'
     }
     else if (response == false) {
+        document.getElementById('ok').innerText = ''
         document.getElementById('cancel').innerText = 'mair khaba😡'
     }
 
