fe01c25be01e05e95249603825798897cae55364
zubayer <zubayer3570@gmail.com>
2022-03-13 18:12:10 +0600

    hi

--- image-slider-constructor/script.js
+++ image-slider-constructor/script.js
@@ -1,14 +1,4 @@
-document.getElementById('login').addEventListener('click', function () {
-    const username = document.getElementById('username').value
-    const password = document.getElementById('password').value
-    if (username == 'zubayer, shona valobashi' && password == 'zubayer ashona kiss koro') {
-        window.location.href = 'index.html'
-    }
-    else {
-        document.getElementById('username').ariaPlaceholder = 'mair khaba, ja bolsi tai lekho'
-        document.getElementById('password').ariaPlaceholder = 'mair khaba, ja bolsi tai lekho'
-    }
-})
+
 
 
 document.getElementById('slider').style.display = 'none'
@@ -16,7 +6,7 @@ document.getElementById('search-btn').addEventListener('click', function () {
     document.getElementById('search-result').innerHTML = ``
     const searchKeyword = document.getElementById('search-keyword').value
     const KEY = '26120190-f11d7a164657ebcc2dd8a2bdc'
-    fetch(`https://pixabay.com/api/?key=${KEY}&q=${searchKeyword}&image_type=all&per_page=200`)
+    fetch(`https://pixabay.com/api/?key=${KEY}&q=${searchKeyword}&image_type=all&per_page=10`)
         .then(res => res.json())
         .then(data => {
             data.hits.forEach(obj => {
